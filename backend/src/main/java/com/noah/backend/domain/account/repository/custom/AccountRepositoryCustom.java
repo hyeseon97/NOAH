5b285c133d3739012e5fe5ae98722e89b8ac1226
오건영 <o54711254@gmail.com>
2024-03-12 17:18:50 +0900

    feat : swagger 기본 설정

---
+++ backend/src/main/java/com/noah/backend/domain/account/repository/custom/AccountRepositoryCustom.java
@@ -0,0 +1,4 @@
+package com.noah.backend.domain.account.repository.custom;
+
+public interface AccountRepositoryCustom {
+}
---
+++ backend/src/main/java/com/noah/backend/global/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.noah.backend.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "NOAH API 명세서",
+                description = "NOAH 서비스 API 명세서",
+                version = "v1"))
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi bankGroup() {
+        return GroupedOpenApi.builder()
+                .group("은행")
+                .pathsToMatch("/api/v1/bank/**")
+                .build();
+    }
+    @Bean
+    public GroupedOpenApi travelGroup() {
+        return GroupedOpenApi.builder()
+                .group("여행")
+                .pathsToMatch("/api/v1/travel/**")
+                .build();
+    }
+}
