2ec36d0ea35dcc75c552c816053cf71aa9a903ba
오건영 <o54711254@gmail.com>
2024-03-17 17:29:30 +0900

    FEAT : account 구조설계

---
+++ backend/src/main/java/com/noah/backend/domain/account/controller/AccountController.java
@@ -0,0 +1,7 @@
+package com.noah.backend.domain.account.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Account 컨트롤러", description = "Account Controller API")
+public class AccountController {
+}
