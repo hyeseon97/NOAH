c6f15ac907e2ed05c76a93a1fea054c9b1db747e
강준규 <wnsrb933@naver.com>
2024-03-18 15:12:13 +0900

    FEAT: 여행 영역 TravelRepository 추가

---
+++ backend/src/main/java/com/noah/backend/domain/travel/dto/requestDto/TravelGetDto.java
@@ -0,0 +1,26 @@
+package com.noah.backend.domain.travel.dto.requestDto;
+
+import com.noah.backend.domain.account.entity.Account;
+import com.noah.backend.domain.memberTravel.entity.MemberTravel;
+import com.noah.backend.domain.notification.entity.Notification;
+import com.noah.backend.domain.plan.entity.Plan;
+import com.noah.backend.domain.ticket.entity.Ticket;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TravelGetDto {
+    private Long id;
+    private String title;
+    private List<MemberTravel> memberTrabelList;
+    private List<Notification> notificationList;
+    private Account account;
+    private Plan plan;
+    private List<Ticket> ticketList;
+}
