cb6358a8c246bab5969a0c5136a3429b5bfb193d
박헤선 <qkr8364@naver.com>
2024-03-18 12:35:41 +0900

    FEAT: API Response 클래스, 응답 코드 enum 클래스 추가

--- backend/src/main/java/com/noah/backend/domain/bank/controller/BankController.java
+++ backend/src/main/java/com/noah/backend/domain/bank/controller/BankController.java
@@ -2,8 +2,6 @@ package com.noah.backend.domain.bank.controller;
 
 import com.noah.backend.domain.bank.dto.RequestHeaderDto;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -15,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/api/v1")
-public class accountController {
+public class BankController {
 	//회원이 있다고 가정
 	@GetMapping("/test/account")
 	@Operation(summary = "계좌 생성", description = "유저 계좌 생성")
