f2a97e8ac0c57294ec615f1d0e1f8ce265d42eb4
오건영 <o54711254@gmail.com>
2024-03-18 17:53:18 +0900

    Test : controller 전체조회 테스트

--- backend/src/main/java/com/noah/backend/domain/account/controller/AccountController.java
+++ backend/src/main/java/com/noah/backend/domain/account/controller/AccountController.java
@@ -1,7 +1,14 @@
 package com.noah.backend.domain.account.controller;
 
+import com.noah.backend.domain.account.service.AccountService;
+import com.noah.backend.global.format.code.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/account")
 public class AccountController {
+
+    private ApiResponse response;
+    private AccountService accountService;
+//    private MemberService memberService;
+
+    @Operation(summary = "내가 속해있는 모임통장 조회", description = "내가 속해있는 모임통장 조회")
+    @GetMapping
+    public ResponseEntity<?> getMyAccountList(@Parameter(hidden = true) Authentication authentication) {
+        return response.success();
+    }
+
 }
