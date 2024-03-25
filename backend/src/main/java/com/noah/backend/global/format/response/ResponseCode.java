a63462eda455200f3269905e9fc59e883c4116ec
이우진 <dldnwls009@gmail.com>
2024-03-25 10:37:58 +0900

    FEAT: 계좌 입금, 계좌 출금, 계좌 이체 구현완료

--- backend/src/main/java/com/noah/backend/global/format/response/ResponseCode.java
+++ backend/src/main/java/com/noah/backend/global/format/response/ResponseCode.java
@@ -45,8 +45,12 @@ public enum ResponseCode {
     TRADE_CREATED(HttpStatus.OK, "거래내역이 성공적으로 생성되었습니다."),
     TRADE_INFO_FETCHED(HttpStatus.OK, "거래내역이 성공적으로 조회되었습니다."),
     TRADE_LIST_NOT_FOUND(HttpStatus.OK, "조회되는 거래내역이 없습니다."),
-    TRADE_UPDATED(HttpStatus.OK, "거래정보가 성공적으로 수정되었습니다.")
+    TRADE_UPDATED(HttpStatus.OK, "거래정보가 성공적으로 수정되었습니다."),
 
+    /* 은행 (Bank) */
+    BANK_DEPOSIT_SUCCESS(HttpStatus.OK,"입금이 성공적으로 완료되었습니다."),
+    BANK_WITHDRAW_SUCCESS(HttpStatus.OK,"출금이 성공적으로 완료되었습니다."),
+    BANK_TRANSFER_SUCCESS(HttpStatus.OK,"이체가 성공적으로 완료되었습니다."),
     ;
 
     /* 여행(Travel) */
