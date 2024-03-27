6df5f54a87d29941822fad1fdc00913de18fc127
오건영 <o54711254@gmail.com>
2024-03-28 00:16:35 +0900

    FEAT : trade, account, groupaccount 컨트롤러 axios 함수 구현

--- frontend/noah/src/api/member/member.js
+++ frontend/noah/src/api/member/member.js
@@ -1,10 +1,11 @@
 import axiosAPI from "../axios";
 const commonUrl = "/api/v1/member/";
 
+/* 로그인 */
 export async function login(object) {
   try {
     const response = await axiosAPI.post(commonUrl + "login", object);
-    console.log("서ㅇ공");
+    console.log("성공");
     return response.data;
   } catch (error) {
     throw error;
