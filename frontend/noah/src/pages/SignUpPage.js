90aa4dd9cf6f88cc7daf40eb2708fc641da70e92
여진구 <dmdkvj369@naver.com>
2024-03-27 17:28:54 +0900

    FIX: 닉네임 중복 검사 추가

--- frontend/noah/src/pages/SignUpPage.js
+++ frontend/noah/src/pages/SignUpPage.js
@@ -4,12 +4,15 @@ import Logo from "../components/common/Logo";
 import styles from "./LoginPage.module.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import App from "./../App";
 
 export default function SignUpPage() {
   const navigate = useNavigate();
   const [signUpFailedMessage, setSignUpFailedMessage] = useState(""); // 로그인 실패시 메시지 변경 "아이디와 비밀번호를 다시 확인해주세요."
   const [isEmailVerifying, setIsEmailVerifying] = useState(false); // 이메일 인증 중인 상태
   const [isEmailVerified, setIsEmailVerified] = useState(false); // 이메일 인증 완료한 상태
+  const [isNickNameVerified, setIsNickNameVerified] = useState(false);
+
   const [isAgreed, setIsAgreed] = useState(true);
   const [buttonText, setButtonText] = useState("이메일 인증");
   const [formData, setFormData] = useState({
@@ -34,7 +37,7 @@ export default function SignUpPage() {
       return;
     }
 
-    if (!isEmailVerified) {
+    if (!isEmailVerified && !isNickNameVerified) {
       // 인증 확인 시도
       // 실패시
       //setSignUpFailedMessage("인증 번호가 올바르지 않습니다.");
@@ -43,7 +46,20 @@ export default function SignUpPage() {
       setIsEmailVerified(true);
       setIsEmailVerifying(false);
       setSignUpFailedMessage("");
+      setButtonText("닉네임 중복 검사");
+
+      return;
+    }
+
+    if (isEmailVerified && !isNickNameVerified) {
+      // 닉네임 중복검사 실시
+      // 실패시
+      // setSignUpFailedMessage("중복된 닉네임입니다.");
+
+      // 성공시
       setButtonText("회원가입");
+      setSignUpFailedMessage("");
+      setIsNickNameVerified(true);
 
       return;
     }
@@ -96,18 +112,23 @@ export default function SignUpPage() {
           {isEmailVerified && (
             <>
               <Input
-                inputType={"password"}
-                placeholderText={"비밀번호"}
-                onChange={handleChange}
-                value={formData.password}
-                name="password"
-              ></Input>
-              <Input
                 inputType={"text"}
                 placeholderText={"닉네임 (한글 2~8자)"}
                 onChange={handleChange}
                 value={formData.nickname}
                 name="nickname"
+              ></Input>
+            </>
+          )}
+
+          {isEmailVerified && isNickNameVerified && (
+            <>
+              <Input
+                inputType={"password"}
+                placeholderText={"비밀번호"}
+                onChange={handleChange}
+                value={formData.password}
+                name="password"
               ></Input>
             </>
           )}
