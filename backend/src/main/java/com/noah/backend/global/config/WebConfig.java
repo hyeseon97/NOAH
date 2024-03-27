bd3a49db4808c319bd377af660919b2f2f7fea93
오건영 <o54711254@gmail.com>
2024-03-27 17:04:36 +0900

    FIX : fix

--- backend/src/main/java/com/noah/backend/global/config/WebConfig.java
+++ backend/src/main/java/com/noah/backend/global/config/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
