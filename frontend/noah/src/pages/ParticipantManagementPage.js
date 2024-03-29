b57e55b46edc57206d61ccff2181cc776cb6a71a
오건영 <o54711254@gmail.com>
2024-03-29 20:49:56 +0900

    STYLE : 인원관리 페이지 CSS

--- frontend/noah/src/pages/ParticipantManagementPage.js
+++ frontend/noah/src/pages/ParticipantManagementPage.js
@@ -1,8 +1,22 @@
 import Header from "./../components/common/Header";
+import styles from "./ParticipantManagementPage.module.css";
 export default function ParticipantManagementPage() {
   return (
     <>
       <Header LeftIcon="Arrow" Title="인원관리" />
+      <div>
+        <div className={styles.memberBox}>
+          <div className={styles.memberBoxLeft}>
+            <div className={styles.labelLarge}>준규</div>
+            <div className={styles.paragraphSmall}>wnsrb123@naver.com</div>
+            <div className={styles.paragraphSmall}>강준규</div>
+          </div>
+          <div className={styles.memberBoxRight}>
+            <div className={styles.paragraphSmall}>2024-03-19 14:28:56</div>
+          </div>
+        </div>
+        <div className={styles.line}></div>
+      </div>
     </>
   );
 }
