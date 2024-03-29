2818ad89897be49f268b5fcd1381536f19061164
오건영 <o54711254@gmail.com>
2024-03-29 21:12:56 +0900

    STYLE : 인원관리 페이지 CSS

--- frontend/noah/src/pages/SpendingManagementPage.js
+++ frontend/noah/src/pages/SpendingManagementPage.js
@@ -3,8 +3,16 @@ import styles from "./SpendingManagementPage.module.css";
 import { ReactComponent as CancelGrey } from "./../assets/Icon/CancelGrey.svg";
 import DropdownMember from "../components/common/DropdownMember";
 import DropdownConsumeType from "../components/common/DropdownConsumeType";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 export default function SpendingManagemnetPage() {
+  useEffect(() => {
+    AOS.init({
+      duration: 200,
+    });
+  });
   return (
     <>
       <Header LeftIcon="Arrow" Title="소비관리" RightIcon="Filter" />
@@ -16,7 +24,7 @@ export default function SpendingManagemnetPage() {
         </div>
       </div>
       <div className={styles.line}></div>
-      <div className={styles.tradeBox}>
+      <div className={styles.tradeBox} data-aos="fade-up">
         <div className={styles.tradeBoxTop}>
           <div className={styles.labelSmallGray}>20:00:13</div>
           <CancelGrey className={styles.icon} />
