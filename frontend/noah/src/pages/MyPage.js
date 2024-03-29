4969557f5cb1d7af7ce035636b9776b46795ddb4
오건영 <o54711254@gmail.com>
2024-03-29 14:35:18 +0900

    TEST : css 작업 중

--- frontend/noah/src/pages/MyPage.js
+++ frontend/noah/src/pages/MyPage.js
@@ -1,8 +1,16 @@
 import Header from "./../components/common/Header";
+import styles from "./MyPage.module.css";
+
 export default function MyPage() {
   return (
     <>
       <Header LeftIcon="Cancel" Title="마이페이지" />
+      <div className={styles.infoContainer}>
+        <div className={styles.labelLarge}>큐티핸섬준규</div>
+        <div className={styles.labelMedium}>강준규</div>
+        <div className={styles.labelSmall}>2024-03-01 가입</div>
+      </div>
+      <div className={styles.menuContainer}>ㅎㅇㅎㅇ</div>
     </>
   );
 }
