f7ea0fc9c8cb633309b2b9316fb6890e5abcf8e4
여진구 <dmdkvj369@naver.com>
2024-03-29 19:12:47 +0900

    FIX: "헤더 뒤로가기 적용"

--- frontend/noah/src/pages/TripPage.js
+++ frontend/noah/src/pages/TripPage.js
@@ -1,8 +1,18 @@
 import Header from "./../components/common/Header";
+import { useNavigate } from "react-router-dom";
+
 export default function TripPage() {
+  const navigate = useNavigate();
+  const handleLeftIconClick = () => {
+    navigate("/home");
+  };
   return (
     <>
-      <Header LeftIcon="Cancel" Title="여행이름" />
+      <Header
+        LeftIcon="Cancel"
+        Title="여행이름"
+        onClick={handleLeftIconClick}
+      />
     </>
   );
 }
