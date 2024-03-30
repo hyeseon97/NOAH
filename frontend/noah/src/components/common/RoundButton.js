dca0355a66e8725ac261f6ef937f2da5abb0058e
오건영 <o54711254@gmail.com>
2024-03-30 21:16:42 +0900

    STYLE : 계획생성페이지 CSS

---
+++ frontend/noah/src/components/common/RoundButton.js
@@ -0,0 +1,36 @@
+export default function Button({ buttonText, warningText }) {
+  const buttonStyle = {
+    /* Lable/Large */
+    fontFamily: "Pretendard",
+    fontSize: "5.9vw",
+    fontWeight: "normal",
+    lineHeight: "150%",
+    textAlign: "center",
+    color: "white",
+    backgroundColor: "black",
+    width: "88vw",
+    height: "13.33vw",
+    margin: "0 auto",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    cursor: "pointer",
+    borderRadius: "2vw",
+  };
+
+  const warningStyle = {
+    fontFamily: "Pretendard",
+    fontStyle: "normal",
+    lineHeight: "160%",
+    color: "#E11900",
+    fontSize: "3.3vw",
+    marginLeft: "6vw",
+  };
+
+  return (
+    <>
+      {warningText && <div style={warningStyle}>{warningText}</div>}
+      <div style={buttonStyle}>{buttonText}</div>
+    </>
+  );
+}
--- frontend/noah/src/pages/PlanningCreatePage.js
+++ frontend/noah/src/pages/PlanningCreatePage.js
@@ -1,9 +1,40 @@
 import Header from "../components/common/Header";
+import styles from "./PlanningCreatePage.module.css";
+import { ReactComponent as Airplane } from "./../assets/Icon/Airplane.svg";
+import { ReactComponent as Mark } from "./../assets/Icon/Mark.svg";
+import { ReactComponent as Calender } from "./../assets/Icon/Calender.svg";
+import RoundButton from "../components/common/RoundButton";
 
 export default function PlanningCreatePage() {
   return (
     <>
       <Header LeftIcon="Arrow" Title="계획 생성" />
+      <div className={styles.planningCreateContainer}>
+        <div className={styles.iconBox}>
+          <Airplane className={styles.icon} />
+        </div>
+        <div className={styles.labelMedium}>여행 국가</div>
+        <div className={styles.setBox}>
+          <Mark className={styles.smallIcon} />
+          <input
+            className={styles.inputBox}
+            type="text"
+            placeholder="장소를 입력하세요"
+          ></input>
+        </div>
+        <div className={styles.labelMedium}>여행 날짜</div>
+        <div className={styles.setBox}>
+          <Calender className={styles.smallIcon} />
+          <input
+            className={styles.inputBox}
+            type="text"
+            placeholder="날짜를 설정하세요"
+          ></input>
+        </div>
+        <div className={styles.buttonBox}>
+          <RoundButton className={styles.button} buttonText="새 계획 생성" />
+        </div>
+      </div>
     </>
   );
 }
