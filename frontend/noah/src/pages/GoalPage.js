5249834920a5596c42321ebfdd13282389024c97
오건영 <o54711254@gmail.com>
2024-03-31 20:55:46 +0900

    FEAT : 모임통장 목표 수정 구현

--- frontend/noah/src/pages/GoalPage.js
+++ frontend/noah/src/pages/GoalPage.js
@@ -3,29 +3,77 @@ import styles from "./GoalPage.module.css";
 import { ReactComponent as Edit } from "./../assets/Icon/Edit.svg";
 import { ReactComponent as GreyPeople } from "./../assets/Icon/GreyPeople.svg";
 import { ReactComponent as BluePeople } from "./../assets/Icon/BluePeople.svg";
+import { ReactComponent as Check } from "./../assets/Icon/check.svg";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getGroupAccount } from "../api/groupaccount/GroupAccount";
+import {
+  getGroupAccount,
+  updateGroupAccountInfo,
+} from "../api/groupaccount/GroupAccount";
 import showToast from "../components/common/Toast";
 
 export default function GoalPage() {
   const { travelId } = useParams();
   const [groupAccountInfo, setGroupAccountInfo] = useState([]);
   const [date, setDate] = useState("");
+  const [editModeTargetAmount, setEditModeTargetAmount] = useState(false);
+  const [editModeTargetDate, setEditModeTargetDate] = useState(false);
+  const [editModePerAmount, setEditModePerAmount] = useState(false);
+  const [editModePaymentDate, setEditModePaymentDate] = useState(false);
+  const [newTargetAmount, setNewTargetAmount] = useState(
+    groupAccountInfo.targetAmount
+  );
+  const [newTargetDate, setNewTargetDate] = useState(
+    groupAccountInfo.targetDate
+  );
+  const [newPerAmount, setNewperAmount] = useState(groupAccountInfo.perAmount);
+  const [newPaymentDate, setNewpaymentDate] = useState(
+    groupAccountInfo.paymentDate
+  );
 
   function formatDate(dateString) {
     const pattern = /(\d{4})(\d{2})(\d{2})/;
     return dateString.replace(pattern, "$1-$2-$3");
   }
 
+  const handleEditTargetAmount = () => {
+    setEditModeTargetAmount(true);
+  };
+  const handleEditTargetDate = () => {
+    setEditModeTargetDate(true);
+  };
+  const handleEditPerAmount = () => {
+    setEditModePerAmount(true);
+  };
+  const handleEditPaymentDate = () => {
+    setEditModePaymentDate(true);
+  };
+
+  // 변경사항 저장 함수
+  const handleSaveChanges = async () => {
+    try {
+      const updatedInfo = {
+        // ...groupAccountInfo, // 기존 정보를 유지하면서
+        groupAccountId: groupAccountInfo.groupAccountId,
+        targetAmount: newTargetAmount, // 새로운 목표금액으로 업데이트
+        targetDate: newTargetDate,
+        perAmount: newPerAmount,
+        paymentDate: newPaymentDate,
+      };
+      await updateGroupAccountInfo(updatedInfo); // API 호출로 정보 업데이트
+      window.location.reload();
+    } catch (error) {
+      console.error("모임통장 정보 업데이트 실패", error);
+      showToast("정보 업데이트에 실패했습니다.");
+    }
+  };
+
   useEffect(() => {
     const fetchGroupAccounts = async () => {
       try {
         const response = await getGroupAccount(travelId);
-        console.log(response);
         if (response.status === "SUCCESS") {
           if (response.data !== null) {
-            console.log("모임통장이 성공적으로 조회되었습니다.");
             setGroupAccountInfo(response.data);
             const dateString = String(response.data.targetDate);
             setDate(formatDate(dateString));
@@ -47,44 +95,127 @@ export default function GoalPage() {
           <div className={styles.goalColumn}>
             <div className={styles.labelSmall}>목표금액</div>
             <div className={styles.amount}>
-              <Edit className={styles.icon} />
-              <div className={styles.labelMedium}>
-                {new Intl.NumberFormat("ko-KR").format(
-                  groupAccountInfo.targetAmount
-                )}{" "}
-                원
-              </div>
+              {editModeTargetAmount ? (
+                <>
+                  <Check className={styles.icon} onClick={handleSaveChanges}>
+                    확인
+                  </Check>
+                  <input
+                    type="number"
+                    value={newTargetAmount}
+                    onChange={(e) => setNewTargetAmount(e.target.value)}
+                    className={styles.inputBox}
+                    placeholder={new Intl.NumberFormat("ko-KR").format(
+                      groupAccountInfo.targetAmount
+                    )}
+                  />
+                </>
+              ) : (
+                <>
+                  <Edit
+                    className={styles.icon}
+                    onClick={handleEditTargetAmount}
+                  />
+                  <div className={styles.labelMedium}>
+                    {new Intl.NumberFormat("ko-KR").format(
+                      groupAccountInfo.targetAmount
+                    )}{" "}
+                    원
+                  </div>
+                </>
+              )}
             </div>
             <div className={styles.line}></div>
           </div>
           <div className={styles.goalColumn}>
             <div className={styles.labelSmall}>목표기간</div>
             <div className={styles.amount}>
-              <Edit className={styles.icon} />
-              <div className={styles.labelMedium}>{date} 일</div>
+              {editModeTargetDate ? (
+                <>
+                  <Check className={styles.icon} onClick={handleSaveChanges}>
+                    확인
+                  </Check>
+                  <input
+                    type="number"
+                    value={newTargetDate}
+                    onChange={(e) => setNewTargetDate(e.target.value)}
+                    className={styles.inputBox}
+                    placeholder={date}
+                    일
+                  />
+                </>
+              ) : (
+                <>
+                  <Edit
+                    className={styles.icon}
+                    onClick={handleEditTargetDate}
+                  />
+                  <div className={styles.labelMedium}>{date} 일</div>
+                </>
+              )}
             </div>
             <div className={styles.line}></div>
           </div>
           <div className={styles.goalColumn}>
             <div className={styles.labelSmall}>월별 납입금액</div>
             <div className={styles.amount}>
-              <Edit className={styles.icon} />
-              <div className={styles.labelMedium}>
-                {new Intl.NumberFormat("ko-KR").format(
-                  groupAccountInfo.perAmount
-                )}{" "}
-                원
-              </div>
+              {editModePerAmount ? (
+                <>
+                  <Check className={styles.icon} onClick={handleSaveChanges}>
+                    확인
+                  </Check>
+                  <input
+                    type="number"
+                    value={newPerAmount}
+                    onChange={(e) => setNewperAmount(e.target.value)}
+                    className={styles.inputBox}
+                    placeholder={new Intl.NumberFormat("ko-KR").format(
+                      groupAccountInfo.perAmount
+                    )}
+                    일
+                  />
+                </>
+              ) : (
+                <>
+                  <Edit className={styles.icon} onClick={handleEditPerAmount} />
+                  <div className={styles.labelMedium}>
+                    {new Intl.NumberFormat("ko-KR").format(
+                      groupAccountInfo.perAmount
+                    )}{" "}
+                    원
+                  </div>
+                </>
+              )}
             </div>
             <div className={styles.line}></div>
           </div>
           <div className={styles.goalColumn}>
             <div className={styles.labelSmall}>납입날짜</div>
             <div className={styles.amount}>
-              <Edit className={styles.icon} />
-              <div className={styles.labelMedium}>
-                {groupAccountInfo.paymentDate}일
-              </div>
+              {editModePaymentDate ? (
+                <>
+                  <Check className={styles.icon} onClick={handleSaveChanges}>
+                    확인
+                  </Check>
+                  <input
+                    type="number"
+                    value={newPaymentDate}
+                    onChange={(e) => setNewpaymentDate(e.target.value)}
+                    className={styles.inputBox}
+                    placeholder={groupAccountInfo.paymentDate}
+                  />
+                </>
+              ) : (
+                <>
+                  <Edit
+                    className={styles.icon}
+                    onClick={handleEditPaymentDate}
+                  />
+                  <div className={styles.labelMedium}>
+                    {groupAccountInfo.paymentDate}일
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </div>
