2fcb1c886eec82b4f4a0319bf2b80a1ee2b5da8e
박헤선 <qkr8364@naver.com>
2024-03-31 22:07:50 +0900

    FIX: 알람 조회에 통화, 환율, 날짜 추가하고 환율 조회는 로그인접근제한 풀었음

--- backend/src/main/java/com/noah/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/noah/backend/global/config/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig {
                     "/api/v1/member/nickname/**",
                     "/api/v1/member/email/**",
                     "/api/v1/member/password-reset",
+                    "api/v1/exchange/rate",
                     "/ws/**",
                     "/pub/**",
                     "/sub/**"
