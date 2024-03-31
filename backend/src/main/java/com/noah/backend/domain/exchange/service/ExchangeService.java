975c0a226ef1713583bfd60d8de3b7f9a04f8ece
박헤선 <qkr8364@naver.com>
2024-03-31 20:23:25 +0900

    FEAT: 환율 정보 조회 추가

--- backend/src/main/java/com/noah/backend/domain/exchange/service/ExchangeService.java
+++ backend/src/main/java/com/noah/backend/domain/exchange/service/ExchangeService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.noah.backend.domain.exchange.dto.requestDto.ExchangeReqDto;
 import com.noah.backend.domain.exchange.dto.responseDto.ExchangeInfoDto;
 
+import com.noah.backend.domain.exchange.dto.responseDto.ExchangeRateGetDto;
 import java.io.IOException;
 
 public interface ExchangeService {
@@ -11,4 +12,6 @@ public interface ExchangeService {
     Long createExchange(String email, ExchangeReqDto exchangeReqDto) throws IOException;
 
     ExchangeInfoDto getExchangeInfo(String email, Long travelId);
+
+    ExchangeRateGetDto getExchangeRate();
 }
