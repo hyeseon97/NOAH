2194417406205c785ef859ce4b923fa2135b05ab
강준규 <wnsrb933@naver.com>
2024-04-01 13:11:51 +0900

    FIX: 항공권 Axios 로직 수정

--- frontend/noah/src/api/flight/flight.js
+++ frontend/noah/src/api/flight/flight.js
@@ -2,18 +2,18 @@ import axiosAPI from "../axios";
 const commonUrl = "/api/v2";
 
 /* 계획 상세 조회 */
-export async function getFlightInfo(planId,
+export async function getFlightInfo(
   originLocationCode,
   destinationLocationCode,
-  departureDate) {
+  departureDate
+) {
   try {
     const params = {
-      planId: planId,
       originLocationCode: originLocationCode,
       destinationLocationCode: destinationLocationCode,
       departureDate: departureDate,
     };
-    const res = await axiosAPI.get(commonUrl + "/flight-offers", { params });
+    const res = await axiosAPI.get(commonUrl + "/flight-offers", {params});
     return res.data;
   } catch (error) {
     throw error;
