3ac25471b7ae68ec6d303a57f8428ba76441cd58
오건영 <o54711254@gmail.com>
2024-04-01 14:42:17 +0900

    FEAT : 알림 끝

--- frontend/noah/src/pages/NotificationPage.js
+++ frontend/noah/src/pages/NotificationPage.js
@@ -1,11 +1,26 @@
-import { getNotification } from "../api/notification/Notification";
+import {
+  deleteNotification,
+  getNotification,
+} from "../api/notification/Notification";
 import Notification from "../components/notification/Notification";
 import Header from "./../components/common/Header";
 import { useEffect, useState } from "react";
+import styles from "./NotificationPage.module.css";
 export default function NotificationPage() {
   const [notifications, setNotifications] = useState([]);
 
-  const handleDelete = (index) => {
+  const handleDelete = (index, notificationId) => {
+    const fetchDeleteNotification = async () => {
+      try {
+        const res = await deleteNotification(notificationId);
+        if (res.status === "SUCCESS") {
+          console.log("삭제성공");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchDeleteNotification();
     setNotifications((currentNotifications) =>
       currentNotifications.filter((_, i) => i !== index)
     );
@@ -16,8 +31,11 @@ export default function NotificationPage() {
       try {
         const res = await getNotification();
         if (res.status === "SUCCESS") {
-          console.log(res.data);
-          setNotifications(res.data);
+          if (res.data === null) {
+          } else {
+            console.log(res.data);
+            setNotifications(res.data);
+          }
         }
       } catch (error) {
         console.log(error);
@@ -29,13 +47,17 @@ export default function NotificationPage() {
   return (
     <>
       <Header LeftIcon="Cancel" Title="알림" />
-      {notifications.map((notification, index) => (
-        <Notification
-          key={index}
-          object={notification}
-          onDelete={() => handleDelete(index)}
-        />
-      ))}
+      {notifications.length > 0 ? (
+        notifications.map((notification, index) => (
+          <Notification
+            key={index}
+            object={notification}
+            onDelete={() => handleDelete(index, notification.notificationId)}
+          />
+        ))
+      ) : (
+        <div className={styles.headingLarge}>조회된 알림이 없습니다.</div>
+      )}
     </>
   );
 }
