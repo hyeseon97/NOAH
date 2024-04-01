44c368c0bfc1169879b4ce778dfd5dee9c1251ae
강준규 <wnsrb933@naver.com>
2024-04-01 21:16:14 +0900

    FIX: planning 페이지 css 모듈화 및 컴포넌트화

--- frontend/noah/src/pages/PlanningPage.js
+++ frontend/noah/src/pages/PlanningPage.js
@@ -6,6 +6,9 @@ import { ReactComponent as TrashCan } from "./../assets/Icon/TrashCan.svg";
 import { ReactComponent as Plus } from "./../assets/Icon/Plus.svg";
 import { ReactComponent as Edit } from "./../assets/Icon/Edit.svg";
 import { ReactComponent as Mark } from "./../assets/Icon/Mark.svg";
+// import DayCalculate from "../components/trip/DayCalculate";
+
+import style from "./PlanningPage.module.css";
 
 import {
   getDetailPlan,
@@ -30,148 +33,148 @@ import {
 //   radius: "5px"
 // }
 
-const headStyle = {
-  height: "50px",
-  margin: "10px",
-  marginTop: "15px",
-  marginBottom: "15px",
-  display: "flex",
-  flexDirection: "row",
-  justifyContent: "space-between",
-  alignItems: "beetween",
-  // backgroundColor: "coral",
-};
+// const headStyle = {
+//   height: "50px",
+//   margin: "10px",
+//   marginTop: "15px",
+//   marginBottom: "15px",
+//   display: "flex",
+//   flexDirection: "row",
+//   justifyContent: "space-between",
+//   alignItems: "beetween",
+//   // backgroundColor: "coral",
+// };
 
-const bigFont = {
-  fontFamily: "Pretendard",
-  fontStyle: "normal",
-  fontWeight: "700",
-  fontSize: "5.2vw",
-  lineHeight: "140%",
-};
+// const bigFont = {
+//   fontFamily: "Pretendard",
+//   fontStyle: "normal",
+//   fontWeight: "700",
+//   fontSize: "5.2vw",
+//   lineHeight: "140%",
+// };
 
-const middleFont = {
-  fontFamily: "Pretendard",
-  fontStyle: "normal",
-  fontWeight: "700",
-  fontSize: "4.6vw",
-  lineHeight: "140%",
-};
+// const middleFont = {
+//   fontFamily: "Pretendard",
+//   fontStyle: "normal",
+//   fontWeight: "700",
+//   fontSize: "4.6vw",
+//   lineHeight: "140%",
+// };
 
-const smallFont = {
-  fontFamily: "Pretendard",
-  fontStyle: "normal",
-  fontWeight: "500",
-  fontSize: "3.9vw",
-  lineHeight: "140%",
-};
+// const smallFont = {
+//   fontFamily: "Pretendard",
+//   fontStyle: "normal",
+//   fontWeight: "500",
+//   fontSize: "3.9vw",
+//   lineHeight: "140%",
+// };
 
-const smallBoldFont = {
-  fontFamily: "Pretendard",
-  fontStyle: "normal",
-  fontWeight: "700",
-  fontSize: "3.9vw",
-  lineHeight: "140%",
-};
+// const smallBoldFont = {
+//   fontFamily: "Pretendard",
+//   fontStyle: "normal",
+//   fontWeight: "700",
+//   fontSize: "3.9vw",
+//   lineHeight: "140%",
+// };
 
-const midStyle = {
-  height: "50px",
-  margin: "10px",
-  display: "flex",
-  flexDirection: "row",
-  justifyContent: "space-between",
-  alignItems: "center",
-  // fontFamily: "Pretendard",
-  // fontStyle: "normal",
-  // backgroundColor: "blue",
-};
+// const midStyle = {
+//   height: "50px",
+//   margin: "10px",
+//   display: "flex",
+//   flexDirection: "row",
+//   justifyContent: "space-between",
+//   alignItems: "center",
+//   // fontFamily: "Pretendard",
+//   // fontStyle: "normal",
+//   // backgroundColor: "blue",
+// };
 
-const boxStyle = {
-  display: "flex",
-  alignItems: "center",
-  border: "1px solid gray",
-  padding: "0px",
-  margin: "10px",
-  borderRadius: "10px",
-  // justifyContent: "space-between"
-};
+// const boxStyle = {
+//   display: "flex",
+//   alignItems: "center",
+//   border: "1px solid gray",
+//   padding: "0px",
+//   margin: "10px",
+//   borderRadius: "10px",
+//   // justifyContent: "space-between"
+// };
 
-const planeBoxStyle = {
-  display: "flex",
-  alignItems: "center",
-  border: "1px solid gray",
-  padding: "0px",
-  margin: "10px",
-  borderRadius: "10px",
-  justifyContent: "space-between",
-  margin: "10px",
-  padding: "25px",
-  height: "100px",
-};
+// const planeBoxStyle = {
+//   display: "flex",
+//   alignItems: "center",
+//   border: "1px solid gray",
+//   padding: "0px",
+//   margin: "10px",
+//   borderRadius: "10px",
+//   justifyContent: "space-between",
+//   margin: "10px",
+//   padding: "25px",
+//   height: "100px",
+// };
 
-// 새로운 계획 추가 버튼 스타일
-const addDetailPlanStyle = {
-  display: "flex",
-  justifyContent: "center", // 수평 중앙 정렬
-  alignItems: "center", // 추가: 수직 중앙 정렬
-  flexDirection: "column",
-  padding: "23px",
-  margin: "10px",
-  border: "1px solid gray",
-  cursor: "pointer",
-  borderRadius: "10px",
-  marginTop: "0px",
-  // backgroundColor: "#f0f0f0",
-  // marginBottom: "10px",
-  // height: "50px",
-};
+// // 새로운 계획 추가 버튼 스타일
+// const addDetailPlanStyle = {
+//   display: "flex",
+//   justifyContent: "center", // 수평 중앙 정렬
+//   alignItems: "center", // 추가: 수직 중앙 정렬
+//   flexDirection: "column",
+//   padding: "23px",
+//   margin: "10px",
+//   border: "1px solid gray",
+//   cursor: "pointer",
+//   borderRadius: "10px",
+//   marginTop: "0px",
+//   // backgroundColor: "#f0f0f0",
+//   // marginBottom: "10px",
+//   // height: "50px",
+// };
 
-const placeInfoStyle = {
-  marginLeft: "15px",
-  width: "170px",
-  // backgroundColor: "RED",
-};
+// const placeInfoStyle = {
+//   marginLeft: "15px",
+//   width: "170px",
+//   // backgroundColor: "RED",
+// };
 
-// 이미지 스타일에 border-radius 추가하여 모서리를 둥글게
-const imgStyle = {
-  width: "90px",
-  height: "90px",
-  margin: "10px",
-  borderRadius: "5px",
-};
+// // 이미지 스타일에 border-radius 추가하여 모서리를 둥글게
+// const imgStyle = {
+//   width: "90px",
+//   height: "90px",
+//   margin: "10px",
+//   borderRadius: "5px",
+// };
 
-const privousButton = {
-  transform: "rotate(90deg)",
-};
+// const privousButton = {
+//   transform: "rotate(90deg)",
+// };
 
-const nextButton = {
-  transform: "rotate(270deg)",
-};
+// const nextButton = {
+//   transform: "rotate(270deg)",
+// };
 
-const editButton = {
-  marginTop: "23px",
-  width: "25px",
-  height: "25px",
-};
+// const editButton = {
+//   marginTop: "23px",
+//   width: "25px",
+//   height: "25px",
+// };
 
-const trashCanButton = {
-  width: "20px",
-  height: "20px",
-  marginTop: "50px",
-  marginLeft: "10px",
-};
+// const trashCanButton = {
+//   width: "20px",
+//   height: "20px",
+//   marginTop: "50px",
+//   marginLeft: "10px",
+// };
 
-const smallPlaneStyle = {
-  width: "30px",
-  height: "30px",
-  // backgroundColor: "green",
-};
+// const smallPlaneStyle = {
+//   width: "30px",
+//   height: "30px",
+//   // backgroundColor: "green",
+// };
 
-const planeInfo = {
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-};
+// const planeInfo = {
+//   display: "flex",
+//   flexDirection: "column",
+//   alignItems: "center",
+// };
 
 const getTimeFromString = (dateTimeString) => {
   const date = new Date(dateTimeString);
@@ -181,6 +184,8 @@ const getTimeFromString = (dateTimeString) => {
 };
 
 export default function PlanningPage() {
+  const [currentDay, setCurrentDay] = useState(1);
+
   const [plan, setPlan] = useState({
     id: 1,
     title: "오사카 일본",
@@ -210,83 +215,174 @@ export default function PlanningPage() {
   const [detailPlans, setDetailPlans] = useState([
     {
       place: "이토모리 신사",
+      day: 1,
       city: "교토, 일본",
       rate: 4.5,
       url: "https://a.cdn-hotels.com/gdcs/production147/d1285/0745ceba-e251-44dd-900d-758bd7078d8a.jpg?impolicy=fcrop&w=800&h=533&q=medium",
     },
     {
       place: "이토모리 호텔",
+      day: 2,
       city: "교토, 일본",
       rate: 4.2,
       url: "https://a.cdn-hotels.com/gdcs/production147/d1285/0745ceba-e251-44dd-900d-758bd7078d8a.jpg?impolicy=fcrop&w=800&h=533&q=medium",
     },
     {
       place: "이토모리 음식점",
+      day: 3,
       city: "교토, 일본",
       rate: 4.1,
       url: "https://a.cdn-hotels.com/gdcs/production147/d1285/0745ceba-e251-44dd-900d-758bd7078d8a.jpg?impolicy=fcrop&w=800&h=533&q=medium",
     },
     {
       place: "독도는 우리땅",
+      day: 4,
       city: "교토, 일본",
       rate: 5.0,
       url: "https://a.cdn-hotels.com/gdcs/production147/d1285/0745ceba-e251-44dd-900d-758bd7078d8a.jpg?impolicy=fcrop&w=800&h=533&q=medium",
     },
   ]);
 
+  const filteredDetailPlans = detailPlans.filter(
+    (detailPlan) => detailPlan.day === currentDay
+  );
+
+  const handleDayChange = (newDay) => {
+    setCurrentDay(newDay);
+  };
+
   return (
     <>
       <Header LeftIcon="Arrow" Title="계획" />
-      <div style={headStyle}>
+      <div className={style.headStyle}>
         <div>
-          <div style={bigFont}>{plan.title}</div>
-          <div style={middleFont}>
+          <div className={style.bigFont}>{plan.title}</div>
+          <div className={style.middleFont}>
             {plan.start_date} ~ {plan.end_date}
           </div>
         </div>
-        <Edit style={editButton} />
+        <Edit className={style.editButton} />
       </div>
-      <div style={midStyle}>
-        <Next style={privousButton} />
+      {/* <div className={style.midStyle}>
+        <Next className={style.privousButton} />
         <div>
-          <div style={smallFont}>
+          <div className={style.smallFont}>
             {day.detailday} {day.detailweekday}
           </div>
-          <div style={bigFont}>DAY {day.sqday}</div>
+          <div className={style.bigFont}>DAY {day.sqday}</div>
         </div>
-        <Next style={nextButton} />
-      </div>
+        <Next className={style.nextButton} />
+      </div> */}
+      <DayCalculate
+        startDate={plan.start_date}
+        endDate={plan.end_date}
+        onDayChange={handleDayChange}
+      />
       <div>
         <div>
-          <div style={planeBoxStyle}>
-            <div style={planeInfo}>
-              <div style={middleFont}>{plane.a_airport}</div>
+          <div className={style.planeBoxStyle}>
+            <div className={style.planeInfo}>
+              <div className={style.middleFont}>{plane.a_airport}</div>
 
-              <div style={smallFont}>{getTimeFromString(plane.arrival)}</div>
+              <div className={style.smallFont}>
+                {getTimeFromString(plane.arrival)}
+              </div>
             </div>
-            <SmallPlane style={smallPlaneStyle} />
-            <div style={planeInfo}>
-              <div style={middleFont}>{plane.d_airport}</div>
-              <div style={smallFont}>{getTimeFromString(plane.departure)}</div>
+            <SmallPlane className={style.smallPlaneStyle} />
+            <div className={style.planeInfo}>
+              <div className={style.middleFont}>{plane.d_airport}</div>
+              <div className={style.smallFont}>
+                {getTimeFromString(plane.departure)}
+              </div>
             </div>
           </div>
-          {detailPlans.map((detailPlan, index) => (
-            <div key={index} style={boxStyle}>
-              <img src={detailPlan.url} style={imgStyle} />
-              <div style={placeInfoStyle}>
-                <div style={smallBoldFont}>{detailPlan.place}</div>
-                <div style={smallFont}>{detailPlan.city}</div>
-                <div style={smallFont}>사용자 평점 {detailPlan.rate}</div>
+          <div>
+            {filteredDetailPlans.map((detailPlan, index) => (
+              <div key={index} className={style.boxStyle}>
+                <img
+                  src={detailPlan.url}
+                  alt="Detail Plan"
+                  className={style.imgStyle}
+                />
+                <div className={style.placeInfoStyle}>
+                  <div className={style.smallBoldFont}>{detailPlan.place}</div>
+                  <div className={style.smallFont}>{detailPlan.city}</div>
+                  <div className={style.smallFont}>
+                    사용자 평점 {detailPlan.rate}
+                  </div>
+                </div>
+                <TrashCan className={style.trashCanButton} />
               </div>
-              <TrashCan style={trashCanButton} />
-            </div>
-          ))}
+            ))}
+            {/* 새로운 계획 추가 버튼 등 나머지 UI 요소 */}
+          </div>
         </div>
-        <div style={addDetailPlanStyle}>
+        <div className={style.addDetailPlanStyle}>
           <Plus />
-          <div style={middleFont}>새로운 계획 추가</div>
+          <div className={style.middleFont}>새로운 계획 추가</div>
         </div>
       </div>
     </>
   );
 }
+
+const DayCalculate = ({ startDate: initialStartDate, endDate: initialEndDate, onDayChange }) => {
+  // useState를 이용하여 초기 상태 설정
+  const [currentDayIndex, setCurrentDayIndex] = useState(0);
+  const [daysList, setDaysList] = useState([]);
+  
+  // 시작 날짜와 종료 날짜를 이용하여 daysList를 계산하는 함수
+  const calculateDays = () => {
+    if (!initialStartDate || !initialEndDate) return;
+
+    const start = new Date(initialStartDate);
+    const end = new Date(initialEndDate);
+    const oneDay = 24 * 60 * 60 * 1000;
+    const differenceInTime = end - start;
+    const differenceInDays = Math.round(differenceInTime / oneDay);
+
+    const newDaysList = [];
+    for (let i = 0; i <= differenceInDays; i++) {
+      newDaysList.push(i + 1); // 실제 day 값은 1부터 시작
+    }
+
+    setDaysList(newDaysList);
+    setCurrentDayIndex(0); // 컴포넌트 내부 인덱스는 0으로 초기화
+    onDayChange(1); // 부모 컴포넌트에는 실제 day 값을 전달
+  };
+
+  // 컴포넌트 마운트 후 calculateDays 함수를 실행
+  useEffect(() => {
+    calculateDays();
+  }, [initialStartDate, initialEndDate]);
+
+  // 이전 Day로 이동
+  const goToPreviousDay = () => {
+    setCurrentDayIndex(prev => {
+      const newIndex = prev > 0 ? prev - 1 : prev;
+      onDayChange(newIndex + 1); // 실제 day 값으로 업데이트
+      return newIndex;
+    });
+  };
+
+  // 다음 Day로 이동
+  const goToNextDay = () => {
+    setCurrentDayIndex(prev => {
+      const newIndex = prev < daysList.length - 1 ? prev + 1 : prev;
+      onDayChange(newIndex + 1); // 실제 day 값으로 업데이트
+      return newIndex;
+    });
+  };
+
+  return (
+    <>
+      {daysList.length > 0 && (
+        <div className={style.midStyle}>
+          <Next className={style.previousButton} onClick={goToPreviousDay} />
+          <div className={style.bigFont}>DAY {daysList[currentDayIndex]}</div>
+          <Next className={style.nextButton} onClick={goToNextDay} />
+        </div>
+      )}
+    </>
+  );
+};
