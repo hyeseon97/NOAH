bd894e666a548888ad4122c9fc1542ae759db60d
강준규 <wnsrb933@naver.com>
2024-04-03 05:16:52 +0900

    FIX: git error 수정

--- frontend/noah/src/pages/GoogleMapSearch.js
+++ frontend/noah/src/pages/GoogleMapSearch.js
@@ -298,19 +298,6 @@ export default function GoogleMapSearch() {
     }
   }, [mapRef.current]);
 
-<<<<<<< HEAD
-  // const myStyle = {
-  //   display: "none",
-  //   flexDirection: "column",
-  //   alignItems: "center",
-  // };
-
-  // const searchList = {
-  //   backgroundColor: "orange",
-  //   height: "5vh",
-  // };
-
-=======
   const handleMapLoad = (map) => {
     mapRef.current = map;
   };
