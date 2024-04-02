4c4c2ab2c943f5732ebf02267f548ed1e3dd6c99
이우진 <dldnwls009@gmail.com>
2024-04-02 17:22:30 +0900

    FIX: 둘러보기할때 제안기능 변수동일하게설정

--- backend/src/main/java/com/noah/backend/domain/suggest/service/SuggestService.java
+++ backend/src/main/java/com/noah/backend/domain/suggest/service/SuggestService.java
@@ -4,9 +4,11 @@ import com.noah.backend.domain.suggest.dto.responseDto.SuggestListResDto;
 import java.util.List;
 
 public interface SuggestService {
-	List<SuggestListResDto> loginGetSuggestList(Long travelId);
+	List<SuggestListResDto> getSuggestList(Long travelId);
 
+	//메인페이지에서 띄울 내가 포함된 여행의 대표 추천 여행
 	List<MainSuggestGetDto> getSuggestMain(String email);
 
-	SuggestListResDto nonLoginGetSuggestList();
+	//둘러보기할때 메인페이지에서 띄울 대표 추천 여행
+	List<MainSuggestGetDto> nonLoginGetSuggestMain();
 }
