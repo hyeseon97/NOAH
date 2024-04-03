f38e5670a6be65906f2463684b3fe58dfddadb87
강준규 <wnsrb933@naver.com>
2024-04-03 17:52:07 +0900

    FEAT: 코멘트 작성 모달 추가

--- frontend/noah/src/components/trip/ReviewModal.js
+++ frontend/noah/src/components/trip/ReviewModal.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
-import {updateComment} from "../../api/comment/Comment"
+import { updateComment } from "../../api/comment/Comment";
+import style from "../trip/ReviewModel.module.css";
 
 Modal.setAppElement("#root"); // 이 부분은 앱의 최상위 컴포넌트에 한 번만 호출되어야 합니다.
 
@@ -9,13 +10,12 @@ function ReviewModal({ isOpen, onRequestClose, travel }) {
 
   const handleSubmit = async () => {
     try {
-      await updateComment(travel.reviewId, content); 
+      await updateComment(travel.reviewId, content);
       onRequestClose();
     } catch (error) {
       console.error(error);
     }
   };
-  
 
   return (
     <Modal
@@ -25,37 +25,44 @@ function ReviewModal({ isOpen, onRequestClose, travel }) {
         content: {
           top: "50%",
           left: "50%",
-          width: "60vw",
-          height: "40vh",
+          width: "63vw",
+          height: "45vh",
           transform: "translate(-50%, -50%)",
         },
       }}
     >
-      <div>{travel.travelTitle}</div>
-      <div>{travel.country}</div>
-      {new Date(travel.startDate)
-        .toLocaleDateString("ko-KR", {
-          year: "numeric",
-          month: "2-digit",
-          day: "2-digit",
-        })
-        .replace(/\.\s?/g, "-")
-        .slice(0, -1)}{" "}
-      ~{" "}
-      {new Date(travel.endDate)
-        .toLocaleDateString("ko-KR", {
-          year: "numeric",
-          month: "2-digit",
-          day: "2-digit",
-        })
-        .replace(/\.\s?/g, "-")
-        .slice(0, -1)}
-      <textarea
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-        style={{ width: "100%", height: "100px" }}
-      />
-      <button onClick={handleSubmit}>후기 제출</button>
+      <div className={style.title}>{travel.travelTitle}</div>
+      <div className={style.country}>{travel.country}</div>
+      <div className={style.date}>
+        {new Date(travel.startDate)
+          .toLocaleDateString("ko-KR", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+          })
+          .replace(/\.\s?/g, "-")
+          .slice(0, -1)}{" "}
+        ~{" "}
+        {new Date(travel.endDate)
+          .toLocaleDateString("ko-KR", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+          })
+          .replace(/\.\s?/g, "-")
+          .slice(0, -1)}
+      </div>
+      <div className={style.textarea}>
+        <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          style={{ width: "50vw", height: "15vh", padding: "1vw", paddingTop:"1vh" }}
+          placeholder="여행 소감을 남겨주세요!"
+        />
+      </div>
+      <button onClick={handleSubmit} className={style.button}>
+        코멘트 작성
+      </button>
     </Modal>
   );
 }
